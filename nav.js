f2dd104504194755167af09e37a3def64817e3c2
Riley Griffis <ragriffis@gmail.com>
2024-03-14 11:43:34 -0400

    Update w/ Nav Bar before I break it

---
+++ nav.js
@@ -0,0 +1,15 @@
+// SEE IT IN DETAILS VIEW !
+
+$(document).ready(function() {
+    
+    console.log('Document ready');
+
+    $('.fa-bars').click(function() {
+        $('.text-menu').toggleClass('menu-appear');
+    });
+
+    $('nav .text-menu a').click(function() {
+        $('.text-menu').toggleClass('menu-appear');
+    });
+    
+});
